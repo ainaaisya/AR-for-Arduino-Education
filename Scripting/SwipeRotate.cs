7bac364cf6c0ed9dd6fc4195a5ec4b4cc4208323
Grace W <aina.azaharee37@gmail.com>
2024-06-07 04:21:18 +0800

    Modify SwipeRotate.cs

--- Scripting/SwipeRotate.cs
+++ Scripting/SwipeRotate.cs
@@ -1,17 +1,16 @@
-using UnityEngine;
+/*using UnityEngine;
 //From Youtube tutorial. Rotate with finger swipe
 
 public class SwipeRotate : MonoBehaviour
 {
 	private Touch touch;
 	private Vector2 touchPosition;
-	private Quaternion rotationY;
 	private float rotateSpeedModifier = 0.1f;
 	
-	//Update is called once per frame
+	//Update is called once per frame 
 	void Update()
 	{
-		if(Input.touchCount > 0)
+        if (Input.touchCount == 1)
 		{
 			touch = Input.GetTouch(0);
 			
@@ -24,15 +23,47 @@ public class SwipeRotate : MonoBehaviour
 		}
 		
 		if (touch.phase == TouchPhase.Moved)
+		{
+			float rotationX = touch.deltaPosition.x * rotateSpeedModifier;
+			float rotationY = touch.deltaPosition.y * rotateSpeedModifier;
+			
+			// Rotate the object around the Y axis based on horizontal swipe
+			transform.Rotate(Vector3.up, -rotationX, Space.World);
+			
+			// Rotate the object around the X axis based on vertical swipe
+			//transform.Rotate(Vector3.right, rotationY, Space.World);
+			transform.Rotate(Camera.main.transform.right, rotationY, Space.World);
+		}
+	}
+}*/
+
+
+using UnityEngine;
+
+public class SwipeRotate : MonoBehaviour
+{
+    private Touch touch;
+    private Vector2 touchPosition;
+    private float rotateSpeedModifier = 0.1f;
+
+    // Update is called once per frame 
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
             {
                 float rotationX = touch.deltaPosition.x * rotateSpeedModifier;
                 float rotationY = touch.deltaPosition.y * rotateSpeedModifier;
-                
+
                 // Rotate the object around the Y axis based on horizontal swipe
-                transform.Rotate(Vector3.up, -rotationX, Space.World);
-				
+                transform.Rotate(Vector3.up, -rotationX, Space.Self);
+
                 // Rotate the object around the X axis based on vertical swipe
-                transform.Rotate(Vector3.right, rotationY, Space.World);
+                transform.Rotate(Vector3.right, rotationY, Space.Self);
             }
-	}
+        }
+    }
 }
